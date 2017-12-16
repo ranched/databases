1eb03c9f3421c985fbaf22bcf79657869dfdea89
ranched <jasosmc@yahoo.com>
2017-12-15 20:05:53 -0600

    Tables created

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,12 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'chats'
+});
+
+exports.connection = connection;
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,96 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+  -- CREATE TABLE messages (
+  --   /* Describe your table here.*/
+  --   chatID int NOT NULL AUTO_INCREMENT,
+  --   userName VARCHAR(32),
+  --   message VARCHAR(32),
+  --   room VARCHAR(32),
+  --   createdAt DATETIME,
+  --   PRIMARY KEY (chatID)  
+  -- );
+
+  -- /* Create other tables and define schemas for them here! */
+
+
+
+
+  -- /*  Execute this file from the command line by typing:
+  --  *    mysql -u root < server/schema.sql
+  --  *  to create the database and the tables.*/
+
+
+  -- -- ---
+  -- -- Globals
+  -- -- ---
+
+  -- -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+  -- -- SET FOREIGN_KEY_CHECKS=0;
+
+  -- -- ---
+  -- -- Table 'messages'
+  -- -- 
+  -- -- ---
+
+/*DROP TABLE IF EXISTS `messages`;*/
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `text` VARCHAR(255) NULL DEFAULT NULL,
+  `id_users` INTEGER NULL DEFAULT NULL,
+  `id_room` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'room'
+-- 
+-- ---
 
+/*DROP TABLE IF EXISTS `room`;*/
+    
+CREATE TABLE `room` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `roomname` VARCHAR(255) NULL DEFAULT 'lobby',
+  PRIMARY KEY (`id`)
+);
 
+-- ---
+-- Table 'users'
+-- 
+-- ---
 
+/*DROP TABLE IF EXISTS `users`;*/
+    
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(32) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+-- ---
+-- Foreign Keys 
+-- ---
 
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (id_room) REFERENCES `room` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `room` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`text`,`id_users`,`id_room`) VALUES
+-- ('','','','');
+-- INSERT INTO `room` (`id`,`roomname`) VALUES
+-- ('','');
+-- INSERT INTO `users` (`id`,`username`) VALUES
+-- ('','');
